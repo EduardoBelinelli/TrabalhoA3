cf8712eadb93e9e11bdeec70819e360370d8b445
Ilmar Junior <82550583+Ilmarjr@users.noreply.github.com>
2024-11-24 14:33:03 -0300

    feat: adding cliente entity

--- src/main/java/entidade/cliente.java
+++ src/main/java/entidade/cliente.java
@@ -1,47 +1,24 @@
 package entidade;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class cliente {
-	private int idade;
+	@Id
+	private Long id;
+	@Column(name = "NAME")
 	private String name;
+	@Column(name = "CPF")
 	private int cpf;
-
-	
-	
-	// get and setters
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		if (idade <= 0) {
-			System.out.println("Ta de sacanagem 0 anos?");
-		}
-		this.idade = idade;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-
-	// construtor
-
-	public cliente(int idade, String name, int cpf) {
-		super();
-		this.idade = idade;
-		this.name = name;
-		this.cpf = cpf;
-	}
 
 }
